ce1040ead44aefe27dd4174114d7d8f2cc68d7fe
Công Nhất <nhatem85@gmail.com>
2025-09-18 17:45:06 +0000

    TÔi deploy code lên vercel nhưng code AI k hoạt động

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: [
+      '@genkit-ai/googleai',
+      'genkit',
+      'firebase-admin',
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
