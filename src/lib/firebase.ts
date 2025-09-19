fa6ac002d1006760c073509ef489b120995cf63c
Công Nhất <nhatem85@gmail.com>
2025-09-19 11:30:31 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,6 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   "projectId": "studio-9320720500-9b879",
   "appId": "1:54917476718:web:397aaa5dd1ab5d0a81517f",
-  "storageBucket": "studio-9320720500-9b879.firebasestorage.app",
   "apiKey": "AIzaSyCb1Pte0GwL9zsQRoZaSfpbidMDqzS2ogU",
   "authDomain": "studio-9320720500-9b879.firebaseapp.com",
   "messagingSenderId": "54917476718"
