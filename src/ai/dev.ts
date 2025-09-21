60defc5b52de3dd0a92d590e3341893d6219cab2
Công Nhất <nhatem85@gmail.com>
2025-09-21 10:07:26 +0000

    Vẫn lỗi

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,3 @@
-import { config } from 'dotenv';
-config({ path: '.env.local' });
-
 import '@/ai/flows/generate-vocabulary-details.ts';
 import '@/ai/flows/interact-with-language-chatbot.ts';
 import '@/ai/flows/generate-batch-vocabulary-details.ts';
