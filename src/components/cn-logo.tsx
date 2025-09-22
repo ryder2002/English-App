f4b345833adf19b195c1195dbd715991eabf1f2d
Công Nhất <nhatem85@gmail.com>
2025-09-22 05:58:37 +0000

    Giờ tôi không cần background nữa hãy xóa đi chữ lại chữ xanh kia và đổi

--- src/components/cn-logo.tsx
+++ src/components/cn-logo.tsx
@@ -8,10 +8,8 @@ interface CNLogoProps {
 
 export function CNLogo({ className }: CNLogoProps) {
   return (
-    <div className={cn("flex items-center gap-2", className)}>
-        <div className="p-1 rounded-xl logo-icon-gradient">
-            <Languages className="h-7 w-7 text-primary" />
-        </div>
+    <div className={cn("flex items-center gap-1.5", className)}>
+        <Languages className="h-8 w-8 logo-icon-gradient text-transparent bg-clip-text" />
         <span className="text-2xl font-bold text-gradient">CN</span>
     </div>
   );
