402eb77e5aa671abb7487ecb45d57c7b31489a49
Công Nhất <nhatem85@gmail.com>
2025-09-22 02:27:49 +0000

    Ý tôi là khi khởi động server là tự compile toàn bộ luôn, Tại vì khi ngư

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -2,6 +2,5 @@ import '@/ai/flows/generate-vocabulary-details.ts';
 import '@/ai/flows/interact-with-language-chatbot.ts';
 import '@/ai/flows/generate-batch-vocabulary-details.ts';
 import '@/ai/flows/generate-quick-vocabulary-details.ts';
-import '@/ai/flows/generate-audio-flow.ts';
 import '@/ai/flows/generate-ipa-flow.ts';
 import '@/ai/flows/generate-pinyin-flow.ts';
