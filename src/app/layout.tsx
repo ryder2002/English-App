3638879f5b17258f9d78f299fc32f310c6ec9be5
Công Nhất <nhatem85@gmail.com>
2025-09-22 11:26:52 +0000

    Tôi vẫn chưa thấy logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "https://www.kaizapp.com/wp-content/uploads/2023/10/icon-instant-translation.svg",
   },
-  manifest: "/manifest.json",
+  manifest: "/manifest.json?v=2",
 };
 
 export default function RootLayout({
@@ -33,7 +33,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="CN" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#0A0F1F" />
       </head>
       <body className={`${inter.variable} font-body antialiased`}>
         <AuthProvider>
