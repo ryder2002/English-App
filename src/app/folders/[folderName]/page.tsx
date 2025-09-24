c7f0eb1bc158a737e3598d0116d9d430900ad2a9
Công Nhất <nhatem85@gmail.com>
2025-09-24 08:38:09 +0000

    Hãy nopm run build tại đây đi

--- src/app/folders/[folderName]/page.tsx
+++ src/app/folders/[folderName]/page.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import { VocabularyFolderList } from "@/components/vocabulary-folder-list";
 import { FolderDetailClient } from "@/components/folder-detail-client";
+import { useParams } from "next/navigation";
 
-export default async function FolderDetailPage({ params }: { params: { folderName: string } }) {
-    const folderName = decodeURIComponent(params.folderName);
+export default function FolderDetailPage() {
+    const params = useParams();
+    const folderName = decodeURIComponent(params.folderName as string);
     
     return (
         <div className="container mx-auto p-4 md:p-6 lg:p-8">
